751dfe24ec3515ccc429c4940bec5c3358278322
Catalina <catalina.garcia.mail@gmail.com>
2021-04-26 17:20:01 +0100

    Adds example structs

--- api/hello.go
+++ api/hello.go
@@ -25,14 +25,35 @@ type Metadata struct {
 
 var m map[string]Metadata
 
+func initialiseVars() Item {
+	exampleItem := Item{
+		[]Metadata{
+			// A page that has already been published
+			{
+				publishTime: time.Date(2020,1,1, 0, 0, 0, 0, time.UTC),
+				content:     "a page",
+			},
+			// A page that will never get published XD
+			{
+				publishTime: time.Now().UTC().Add(time.Second * 15),
+				content:     "a second page",
+			},
+		},
+	}
+
+	return exampleItem
+}
+
 // HelloHandler returns function containing a simple hello world example of an api handler
 func HelloHandler(ctx context.Context) http.HandlerFunc {
 	log.Event(ctx, "api contains example endpoint, remove hello.go as soon as possible", log.INFO)
 	return func(w http.ResponseWriter, req *http.Request) {
 		ctx := req.Context()
 
+		item := initialiseVars()
+
 		response := HelloResponse{
-			Message: req.URL.String(),
+			Message:  req.URL.String(),
 		}
 
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
